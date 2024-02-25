f9ddbfcb30ab9aa3b41d7f7ef45d0e39de402e77
Alan <alanrd772@gmail.com>
2024-02-25 17:47:47 +0400

    Add focal length breakdown display + styling improvements

--- renderer.js
+++ renderer.js
@@ -2,6 +2,7 @@ dirButton = document.getElementById("openpicker")
 statusH = document.getElementById("status")
 makeModelData = document.getElementById("makemodeldata")
 modelData = document.getElementById("modeldata")
+lengths35Data = document.getElementById("flength35data")
 
 dirButton.addEventListener("click", async e => {
     statusH.innerText = "Loading photos..."
@@ -19,11 +20,9 @@ dirButton.addEventListener("click", async e => {
     }
 
     makes = data.makes
-    lenses = data.lenses
-
     for (makeName of Object.keys(makes)) {
         make = makes[makeName]
-        // get the contents of the <template> for a camera make and make>model cards
+        // get the contents of the <template> for camera make and make>model cards
         maketmpl = document.getElementById("makeprog").content.cloneNode(1).firstElementChild
 
         maketmpl.getElementsByTagName("progress")[0].value = 100*(make.count/data.total)
@@ -33,15 +32,15 @@ dirButton.addEventListener("click", async e => {
         maketmpl.getElementsByClassName("make")[0].appendChild(makeTxt)
 
         console.log(make.models, Object.keys(make.models))
-        for (modelName of Object.keys(make.models)) {
-            model = make.models[modelName]
+        for (model of Object.keys(make.models)) {
+            modelCount = make.models[model]
 
             mmodeltmpl = document.getElementById("mmodelprog").content.cloneNode(1).firstElementChild
 
-            mmodeltmpl.getElementsByTagName("progress")[0].value = 100*(model/make.count)
-            count = document.createTextNode(model)
+            mmodeltmpl.getElementsByTagName("progress")[0].value = 100*(modelCount/make.count)
+            count = document.createTextNode(modelCount)
             mmodeltmpl.getElementsByClassName("count")[0].appendChild(count)
-            modelNameTxt = document.createTextNode(modelName)
+            modelNameTxt = document.createTextNode(model)
             mmodeltmpl.getElementsByClassName("model")[0].appendChild(modelNameTxt)
             // add this model progress bar to the make
             maketmpl.getElementsByClassName("models")[0].appendChild(mmodeltmpl)
@@ -49,10 +48,10 @@ dirButton.addEventListener("click", async e => {
 
             modeltmpl = document.getElementById("modelprog").content.cloneNode(1).firstElementChild
 
-            modeltmpl.getElementsByTagName("progress")[0].value = 100*(model/data.total)
-            count = document.createTextNode(model)
+            modeltmpl.getElementsByTagName("progress")[0].value = 100*(modelCount/data.total)
+            count = document.createTextNode(modelCount)
             modeltmpl.getElementsByClassName("count")[0].appendChild(count)
-            makeAndModelNameTxt = document.createTextNode(makeName + " " + modelName)
+            makeAndModelNameTxt = document.createTextNode(makeName + " " + model)
             modeltmpl.getElementsByClassName("model")[0].appendChild(makeAndModelNameTxt)
             // add this make&model progress bar to the models
             modelData.appendChild(modeltmpl)
@@ -60,4 +59,20 @@ dirButton.addEventListener("click", async e => {
 
         makeModelData.appendChild(maketmpl)
     }
+
+    fLengths35 = data.focalLengths35
+    for (length of Object.keys(fLengths35)) {
+        lengthCount = fLengths35[length]
+        lentmpl = document.getElementById("flength35prog").content.cloneNode(1).firstElementChild
+
+        lentmpl.getElementsByTagName("progress")[0].value = 100*(lengthCount/data.total)
+        count = document.createTextNode(lengthCount)
+        lentmpl.getElementsByClassName("count")[0].appendChild(count)
+        lenTxt = document.createTextNode(length + " mm")
+        lentmpl.getElementsByClassName("flength35")[0].appendChild(lenTxt)
+
+        lengths35Data.appendChild(lentmpl)
+    }
+    
+    lenses = data.lenses
 })
